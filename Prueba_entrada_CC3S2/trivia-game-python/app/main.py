9ceae757ee07ef1933fdd27b0a8c909927269344
= <=>
2025-04-11 03:02:18 -0500

    Dia 2: Implementacion de la clase Question y pruebas unitarias simples

--- Prueba_entrada_CC3S2/trivia-game-python/app/main.py
+++ Prueba_entrada_CC3S2/trivia-game-python/app/main.py
@@ -8,5 +8,3 @@ app = FastAPI()
 async def root():
     return {"mensaje": "Bienvenido al juego de trivia"}
     
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
